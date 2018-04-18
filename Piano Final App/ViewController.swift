e3ff2073ca0e499db3de4d1fbb14503736f0a4a7
Ryan Cummins <rcummins@ryans-macbook-air-62.local>
2018-04-18 10:38:39 -0500

    add tap gestures for all the keys and made them actions

--- Piano Final App/ViewController.swift
+++ Piano Final App/ViewController.swift
@@ -12,14 +12,49 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    /////////White Keys///////
+    @IBAction func F(_ sender: UITapGestureRecognizer) {
+        
     }
-
-
+    
+    @IBAction func G(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func A(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func B(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func C(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func D(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func E(_ sender: UITapGestureRecognizer) {
+    }
+    ////////////////////////
+    //////Black Keys///////
+    @IBAction func FSharpKey(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func GSharpKey(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func ASharpKey(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func CSharpKey(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func DSharpKey(_ sender: UITapGestureRecognizer) {
+    }
+    
+    
+    
 }
 
