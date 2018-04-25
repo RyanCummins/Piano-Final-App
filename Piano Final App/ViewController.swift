3a7df2977e167dbded5c552a975f4236ca4967fb
Ryan Cummins <rcummins@ryans-macbook-air-62.local>
2018-04-25 11:23:45 -0500

    Organized Code Added more code - working on changing the color of the buttons when pressed.... not done yet

--- Piano Final App/ViewController.swift
+++ Piano Final App/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 import AVFoundation
 class ViewController: UIViewController {
-    
+ //////////////Variables//////////////
     var sound = AVAudioPlayer()
+    var buttonTapped = Bool()
     
-    
+///////////Keys as Outlets////////
     @IBOutlet weak var FKeyLabel: UIView!
     @IBOutlet weak var GKeyLabel: UIView!
     @IBOutlet weak var AKeyLabel: UIView!
@@ -24,63 +25,90 @@ class ViewController: UIViewController {
     @IBOutlet weak var ASharpKeyLabel: UIView!
     @IBOutlet weak var CSharpKeyLabel: UIView!
     @IBOutlet weak var DSharpKeyLabel: UIView!
-    
+    @IBOutlet weak var octaveSlider: UISlider!
+    //////////Overrid func//////////
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    ////////////Touch Functions//////
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        FKeyLabel.backgroundColor = UIColor.cyan
+        GKeyLabel.backgroundColor = UIColor.cyan
+        AKeyLabel.backgroundColor = UIColor.cyan
+        BKeyLabel.backgroundColor = UIColor.cyan
+        CKeyLabel.backgroundColor = UIColor.cyan
+        DKeyLabel.backgroundColor = UIColor.cyan
+        EKeyLabel.backgroundColor = UIColor.cyan
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        FKeyLabel.backgroundColor = UIColor.white
+        GKeyLabel.backgroundColor = UIColor.white
+        AKeyLabel.backgroundColor = UIColor.white
+        BKeyLabel.backgroundColor = UIColor.white
+        CKeyLabel.backgroundColor = UIColor.white
+        DKeyLabel.backgroundColor = UIColor.white
+        EKeyLabel.backgroundColor = UIColor.white
+    }
+    //////////////Sound Effects/////
+    @IBAction func octaveSliderAction(_ sender: UISlider) {
+        
+    }
     
-    /////////White Keys///////
+    @IBAction func soundChanger(_ sender: UISegmentedControl) {
+    }
+    
+   ////////////White Keys///////////
     @IBAction func F(_ sender: UITapGestureRecognizer) {
-        FKeyLabel.backgroundColor = UIColor.darkGray
+        FKeyLabel.backgroundColor = UIColor.white
     }
     
     @IBAction func G(_ sender: UITapGestureRecognizer) {
-        GKeyLabel.backgroundColor = UIColor.darkGray
+        GKeyLabel.backgroundColor = UIColor.white
     }
     
     @IBAction func A(_ sender: UITapGestureRecognizer) {
-        AKeyLabel.backgroundColor = UIColor.darkGray
+        AKeyLabel.backgroundColor = UIColor.white
     }
     
     @IBAction func B(_ sender: UITapGestureRecognizer) {
-        BKeyLabel.backgroundColor = UIColor.darkGray
+        BKeyLabel.backgroundColor = UIColor.white
     }
     
     @IBAction func C(_ sender: UITapGestureRecognizer) {
-        CKeyLabel.backgroundColor = UIColor.darkGray
+        CKeyLabel.backgroundColor = UIColor.white
         ButtonPressC()
     }
     
     @IBAction func D(_ sender: UITapGestureRecognizer) {
-        DKeyLabel.backgroundColor = UIColor.darkGray
+        DKeyLabel.backgroundColor = UIColor.white
     }
     
     @IBAction func E(_ sender: UITapGestureRecognizer) {
-        EKeyLabel.backgroundColor = UIColor.darkGray
+        EKeyLabel.backgroundColor = UIColor.white
     }
-    ////////////////////////
-    //////Black Keys///////
+    
+    //////Black Keys////////////////
     @IBAction func FSharpKey(_ sender: UITapGestureRecognizer) {
-        FSharpKeyLabel.backgroundColor = UIColor.darkGray
+        FSharpKeyLabel.backgroundColor = UIColor.black
     }
     
     @IBAction func GSharpKey(_ sender: UITapGestureRecognizer) {
-        GSharpKeyLabel.backgroundColor = UIColor.darkGray
+        GSharpKeyLabel.backgroundColor = UIColor.black
     }
     
     @IBAction func ASharpKey(_ sender: UITapGestureRecognizer) {
-        ASharpKeyLabel.backgroundColor = UIColor.darkGray
+        ASharpKeyLabel.backgroundColor = UIColor.black
     }
     
     @IBAction func CSharpKey(_ sender: UITapGestureRecognizer) {
-        CSharpKeyLabel.backgroundColor = UIColor.darkGray
+        CSharpKeyLabel.backgroundColor = UIColor.black
     }
     
     @IBAction func DSharpKey(_ sender: UITapGestureRecognizer) {
-        DSharpKeyLabel.backgroundColor = UIColor.darkGray
+        DSharpKeyLabel.backgroundColor = UIColor.black
     }
-    
+    /////////Sound Functions////////
     func ButtonPressC() {
         if let path = Bundle.main.path(forResource: "Note C #1 - 4:4:18, 10.57 AM", ofType: "mp3") {
             let url = URL(fileURLWithPath: path)
@@ -93,6 +121,6 @@ class ViewController: UIViewController {
             }
         }
     }
-    
+   ///////////////////////////////////
 }
 
