b9f5ce00b97dad13f92612a70d1afe06107d0db8
Ryan Cummins <rcummins@ryans-macbook-air-62.local>
2018-05-01 10:46:07 -0500

    Made buttons for everything, added a beginner mode, and added colors to buttons when pressed

--- Piano Final App/ViewController.swift
+++ Piano Final App/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     var buttonTapped = Bool()
     
     ///////////Keys as Outlets////////
-  
+    
     @IBOutlet weak var FKey: UIButton!
     @IBOutlet weak var GKey: UIButton!
     @IBOutlet weak var AKey: UIButton!
@@ -45,22 +45,138 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
+   /////////Actions Pressed Down////////
+    
+    @IBAction func FKeyPressedDown(_ sender: UIButton) {
+        FKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func GKeyPressedDown(_ sender: UIButton) {
+        GKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func AKeyPressedDown(_ sender: UIButton) {
+        AKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func BKeyPressedDown(_ sender: UIButton) {
+        BKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func CKeyPressedDown(_ sender: UIButton) {
+        CKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func DKeyPressedDown(_ sender: UIButton) {
+        DKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func EKeyPressedDown(_ sender: UIButton) {
+        EKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func FSharpKeyPressedDown(_ sender: UIButton) {
+        FSharpKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func GSharpKeyPressedDown(_ sender: UIButton) {
+        GSharpKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func ASharpKeyPressedDown(_ sender: UIButton) {
+        ASharpKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func CSharpKeyPressedDown(_ sender: UIButton) {
+        CSharpKey.backgroundColor = UIColor.cyan
+    }
+    
+    @IBAction func DSharpKeyPressedDown(_ sender: UIButton) {
+        DSharpKey.backgroundColor = UIColor.cyan
+    }
+    
+    
+  /////////Actions Touched Up//
+    
+    @IBAction func FKeyLiftUp(_ sender: UIButton) {
+        FKey.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func GKeyLiftUp(_ sender: UIButton) {
+        GKey.backgroundColor = UIColor.white
+    }
+    @IBAction func AKeyLiftUp(_ sender: UIButton) {
+        AKey.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func BKeyLiftUp(_ sender: UIButton) {
+        BKey.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func CKeyLiftUp(_ sender: UIButton) {
+        CKey.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func DKeyLiftUp(_ sender: UIButton) {
+        DKey.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func EKeyLiftUp(_ sender: UIButton) {
+        EKey.backgroundColor = UIColor.white
+    }
    
+    @IBAction func FSharpKeyLiftUp(_ sender: UIButton) {
+        FSharpKey.backgroundColor = UIColor.black
+    }
+    
+    @IBAction func GSharpKeyLiftUp(_ sender: UIButton) {
+        GSharpKey.backgroundColor = UIColor.black
+    }
+    
+    @IBAction func ASharpKeyLiftUp(_ sender: UIButton) {
+        ASharpKey.backgroundColor = UIColor.black
+    }
+    
+    @IBAction func CSharpKeyLiftUp(_ sender: UIButton) {
+        CSharpKey.backgroundColor = UIColor.black
+    }
+    
+    @IBAction func DSharpKeyLiftUp(_ sender: UIButton) {
+        DSharpKey.backgroundColor = UIColor.black
+    }
+    
+    
+    //////Extra Actions///////
     @IBAction func octaveSliderAction(_ sender: UISlider) {
         
     }
     
     @IBAction func easyToHard(_ sender: UISegmentedControl) {
+        var result: String
         switch segmentedController.selectedSegmentIndex {
-       case 0:
-         FKey.text = "F"
+        case 0:
+            FKeyLabel.text = ""
+            GKeyLabel.text = ""
+            AKeyLabel.text = ""
+            BKeyLabel.text = ""
+            CKeyLabel.text = ""
+            DKeyLabel.text = ""
+            EKeyLabel.text = ""
+        case 1:
+            FKeyLabel.text = "F"
+            GKeyLabel.text = "G"
+            AKeyLabel.text = "A"
+            BKeyLabel.text = "B"
+            CKeyLabel.text = "C"
+            DKeyLabel.text = "D"
+            EKeyLabel.text = "E"
         default:
-            result = 0
+            result = ""
         }
     }
-
     
     
-
+    
+    
 }
 
